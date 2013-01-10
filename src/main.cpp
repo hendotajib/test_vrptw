cc28954b7d46aa88cee947dfab425daca364c3f3
Konstantin Zhidovinov <kzhidovinov@gmail.com>
2013-01-10 09:34:22 +0400

    add test file

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,13 @@
 
 static InstanceData g_InstanceData;
 int main(int argc, char** argv){
+	g_InstanceData.read("D:/Projects/test_vrptw/c101_25.txt");
+	SolutionLogger *logger = new SolutionLogger();
+	VrptwMACS vrp(&g_InstanceData, logger);
+	vrp.run(10);
+	logger->write("log.txt");
+	int vehicle;
+	double dist;
+	int *tours = g_InstanceData.getSolutionTours(&vehicle, &dist);
 	return 0;
 }
